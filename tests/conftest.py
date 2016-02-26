6d69ccb7854c4412cf951007209a163640d91b20
ejhumphrey <humphrey.eric@gmail.com>
2016-02-26 10:35:52 -0500

    Rearranged test files.

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+import shutil
+import tempfile
+
+
+@pytest.fixture()
+def workspace(request):
+    test_workspace = tempfile.mkdtemp()
+
+    def fin():
+        if os.path.exists(test_workspace):
+            shutil.rmtree(test_workspace)
+
+    request.addfinalizer(fin)
+
+    return test_workspace
---
+++ tests/test_common_utils.py
@@ -0,0 +1,3 @@
+import pytest
+
+import common.utils as utils
