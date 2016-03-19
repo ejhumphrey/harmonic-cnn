95dfd0c8c5f80be81dc706750f01223c1503914f
Christopher Jacoby <cjacoby@museami.com>
2016-03-18 20:00:39 -0400

    print model selection results

--- wcqtlib/train/driver.py
+++ wcqtlib/train/driver.py
@@ -407,7 +407,9 @@ def find_best_model(config, experiment_name, validation_df, plot_loss=False):
                                  original_config['experiment/best_params'])
         shutil.copyfile(best_model['model_file'], best_path)
     else:
+        logger.info("Model Search already done; printing previous results")
         result_df = pandas.read_pickle(validation_error_file)
+        logger.info(result_df)
 
     if plot_loss:
         fig = plt.figure()
