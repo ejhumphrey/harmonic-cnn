8f03f9325470480b4071ecebe56c197254c116cc
Christopher Jacoby <cjacoby@museami.com>
2016-03-22 21:38:05 -0400

    write them to a file.'

--- manage.py
+++ manage.py
@@ -227,7 +227,8 @@ def datatest(master_config, show_full=False):
         print(utils.colored("You're all set; your dataset matches.", "green"))
 
     print(utils.colored("Now checking all files for validity."))
-    bad_files = E.check_valid_audio_files(datasets_df)
+    bad_files = E.check_valid_audio_files(datasets_df,
+                                          write_path="bad_file_reads.txt")
     print(utils.colored("{} files could not be opened", "red"))
     if bad_files:
         print("Unable to open the following audio files:")
