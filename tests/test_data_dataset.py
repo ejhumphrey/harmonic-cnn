d201f2725c7987b80c86ec1acb8952fd31091812
Christopher Jacoby <cjacoby@museami.com>
2016-03-25 17:13:01 -0400

    starting to work on importing data from old way to new way.

--- tests/test_data_dataset.py
+++ tests/test_data_dataset.py
@@ -1,13 +1,27 @@
+import os
 import pytest
 
+import wcqtlib.common.config as C
 import wcqtlib.data.dataset as dataset
 
+CONFIG_PATH = os.path.join(os.path.dirname(__file__),
+                           os.pardir, "data", "master_config.yaml")
 
-def test_csv():
+
+@pytest.fixture(scope="module")
+def config():
+    return C.Config.from_yaml(CONFIG_PATH)
+
+
+def test_get_remote_schema():
     pass
 
 
-def test_pickle():
+def test_observation():
+    pass
+
+
+def test_json():
     pass
 
 
@@ -15,9 +29,19 @@ def test_to_df():
     pass
 
 
-def test_to_dict():
+def test_to_builtin():
     pass
 
 
 def test_dataset_view():
     pass
+
+
+def build_tiny_dataset_from_old_dataframe(config):
+    tinyds = dataset.build_tiny_dataset_from_old_dataframe(config)
+
+    assert isinstance(tinyds, list)
+    assert len(tinyds) == 36
+
+    schema = dataset.get_remote_schema()
+    assert all([obs.validate(schema) for obs in tinyds])
--- wcqtlib/data/dataset.py
+++ wcqtlib/data/dataset.py
@@ -2,8 +2,57 @@
 """
 
 import copy
+import json
+import jsonschema
 import os
 import pandas
+import requests
+
+
+SCHEMA_PATH = "https://raw.githubusercontent.com/ejhumphrey/minst-dataset/" \
+              "master/minst/schema/observation.json"
+
+
+def get_remote_schema(url=SCHEMA_PATH):
+    return requests.get(url).json()
+
+
+class Observation(object):
+    """Document model each item in the collection.
+    TODO: Inherit / whatever from minst-dataset repo
+    """
+    SCHEMA = json.load(open(SCHEMA_PATH))
+
+    def __init__(self, index, dataset, audio_file, instrument, source_key,
+                 start_time, duration, note_number, dynamic, partition,
+                 features=None):
+        self.index = index
+        self.dataset = dataset
+        self.audio_file = audio_file
+        self.instrument = instrument
+        self.source_key = source_key
+        self.start_time = start_time
+        self.duration = duration
+        self.note_number = note_number
+        self.dynamic = dynamic
+        self.partition = partition
+        self.features = features if features else dict()
+
+    def to_dict(self):
+        return self.__dict__.copy()
+
+    def validate(self, schema=None):
+        schema = self.SCHEMA if schema is None else schema
+        success = True
+        try:
+            jsonschema.validate(self.to_dict(), schema)
+        except jsonschema.ValidationError:
+            success = False
+        success &= os.path.exists(self.audio_file)
+        # if success:
+        #     success &= utils.check_audio_file(self.audio_file)[0]
+
+        return success
 
 
 class Dataset(object):
@@ -23,14 +72,14 @@ class Dataset(object):
         pass
 
     @classmethod
-    def read_csv(self, csv_path):
+    def read_json(self, csv_path):
         pass
 
     @classmethod
     def read_pickle(self, pickle_path):
         pass
 
-    def save_csv(self, csv_path):
+    def save_json(self, csv_path):
         pass
 
     def save_pickle(self, pickle_path):
@@ -42,7 +91,7 @@ class Dataset(object):
         return self._df
 
     @property
-    def to_dict(self):
+    def to_builtin(self):
         return self._to_dict
 
     def view(self, dataset):
@@ -50,3 +99,31 @@ class Dataset(object):
         thecopy = copy.copy(self)
         thecopy.set_test_set(dataset)
         return thecopy
+
+
+def build_tiny_dataset_from_old_dataframe(config):
+    notes_df_path = os.path.join(
+        os.path.expanduser(config['paths/data_dir']),
+        config['dataframes/notes'])
+    notes_df = pandas.read_pickle(notes_df_path)
+
+    tiny_dataset = []
+    # Get one file for each instrument for each dataset.
+    for dataset in notes_df["dataset"].unique():
+        for instrument in notes_df["instrument"].unique():
+            record = notes_df.loc[(notes_df["dataset"] == dataset) &
+                                  (notes_df["instrument"] == instrument)][0]
+            tiny_dataset.append(
+                Observation(
+                    index=record.index,
+                    dataset=record['dataset'],
+                    audio_file=record['audio_file'],
+                    instrument=record['instrument'],
+                    source_key=None,
+                    start_time=None,
+                    duration=None,
+                    note_number=None,
+                    dynamic=None,
+                    partition=None
+                    ))
+    return tiny_dataset
