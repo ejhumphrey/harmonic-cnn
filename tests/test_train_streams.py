36fffc9f5ba05823bd2532d6a390ee86f6643e11
Christopher Jacoby <cjacoby@museami.com>
2016-05-06 17:22:16 -0700

    streammer tests updated and passing (other than zmq :( )

--- tests/test_train_streams.py
+++ tests/test_train_streams.py
@@ -18,8 +18,7 @@ def tiny_feats(module_workspace, tinyds):
         limited_ds, module_workspace, skip_existing=True)
 
 
-def __test_cqt_slicer(dataset, t_len, *slicer_args):
-    result = []
+def __assert_cqt_slicer(dataset, t_len, *slicer_args):
     slicer = streams.cqt_slices(dataset.to_df().iloc[0], t_len,
                                 *slicer_args)
     for i in range(10):
@@ -27,8 +26,6 @@ def __test_cqt_slicer(dataset, t_len, *slicer_args):
         assert len(data.shape) == 4
         assert data.shape[1] == 1
         assert data.shape[2] == t_len
-
-    return all(result)
 
 
 def __assert_wcqt_slicer(dataset, t_len, *slicer_args):
@@ -51,74 +48,55 @@ def __assert_hcqt_slicer(dataset, t_len, *slicer_args):
         assert data.shape[2] == t_len
 
 
-def test_cqt_slices(tiny_feats):
-    __test_cqt_slicer(tiny_feats, 5)
-    __test_cqt_slicer(tiny_feats, 1)
-    __test_cqt_slicer(tiny_feats, 10)
-    __test_cqt_slicer(tiny_feats, 8, False, False)
+@pytest.mark.parametrize(
+    "slicer_test",
+    [__assert_cqt_slicer,
+     __assert_wcqt_slicer,
+     __assert_hcqt_slicer],
+    ids=["cqt", "wcqt", "hcqt"])
+def test_slices(slicer_test, tiny_feats):
+    slicer_test(tiny_feats, 5)
+    slicer_test(tiny_feats, 1)
+    slicer_test(tiny_feats, 10)
+    slicer_test(tiny_feats, 8, False, False)
 
 
-def test_wcqt_slices(tiny_feats):
-    __assert_wcqt_slicer(tiny_feats, 5)
-    __assert_wcqt_slicer(tiny_feats, 1)
-    __assert_wcqt_slicer(tiny_feats, 10)
-    __assert_wcqt_slicer(tiny_feats, 8, False, False)
+def __test_streamer(streamer, t_len, batch_size):
+    counter = 0
+    while counter < 5:
+        batch = next(streamer)
+        assert batch is not None
+        assert 'x_in' in batch and 'target' in batch
+        assert len(batch['x_in'].shape) == 4
+        assert len(batch['target'].shape) == 1
+        assert batch['x_in'].shape[2] == t_len
+        assert batch['x_in'].shape[0] == batch_size
+        assert batch['target'].shape[0] == batch_size
+        print("Result batch size:", batch['x_in'].shape[0])
+        counter += 1
 
 
-def test_hcqt_slices(tiny_feats):
-    __assert_hcqt_slicer(tiny_feats, 5)
-    __assert_hcqt_slicer(tiny_feats, 1)
-    __assert_hcqt_slicer(tiny_feats, 10)
-    __assert_hcqt_slicer(tiny_feats, 8, False, False)
+@pytest.mark.parametrize("slicer", [streams.cqt_slices,
+                                    streams.wcqt_slices,
+                                    streams.hcqt_slices],
+                         ids=["cqt", "wcqt", "hcqt"])
+def test_instrument_streamer(slicer, tiny_feats):
+    df = tiny_feats.to_df()
+    t_len = 10
+    batch_size = 12
+    if not df.empty:
+        streamer = streams.InstrumentStreamer(
+            df, slicer, t_len=t_len, batch_size=batch_size)
+        __test_streamer(streamer, t_len, batch_size)
 
 
-# @pytest.mark.xfail(reason="zmq is always generating batches of only one.")
-# def __test_streamer(streamer, t_len, batch_size):
-#     counter = 0
-#     while counter < 5:
-#         batch = next(streamer)
-#         assert batch is not None
-#         assert 'x_in' in batch and 'target' in batch
-#         assert len(batch['x_in'].shape) == 4
-#         assert len(batch['target'].shape) == 1
-#         assert batch['x_in'].shape[2] == t_len
-#         assert batch['x_in'].shape[0] == batch_size
-#         assert batch['target'].shape[0] == batch_size
-#         print("Result batch size:", batch['x_in'].shape[0])
-#         counter += 1
-
-
-# def test_instrument_streamer_cqt(tiny_feats):
-#     df = tiny_feats.to_df()
-#     t_len = 10
-#     batch_size = 12
-#     datasets = ["rwc"]
-#     if not df.empty:
-#         streamer = streams.InstrumentStreamer(
-#             df, datasets, streams.cqt_slices,
-#             t_len=t_len, batch_size=batch_size)
-#         yield __test_streamer, streamer, t_len, batch_size
-
-
-# def test_instrument_streamer_wcqt(tiny_feats):
-#     df = tiny_feats.to_df()
-#     t_len = 10
-#     batch_size = 12
-#     datasets = ["rwc"]
-#     if not df.empty:
-#         streamer = streams.InstrumentStreamer(
-#             df, datasets, streams.wcqt_slices,
-#             t_len=t_len, batch_size=batch_size)
-#         yield __test_streamer, streamer, t_len, batch_size
-
-
-# def test_instrument_streamer_with_zmq(tiny_feats):
-#     df = tiny_feats.to_df()
-#     t_len = 10
-#     batch_size = 12
-#     datasets = ["rwc"]
-#     if not df.empty:
-#         streamer = streams.InstrumentStreamer(
-#             df, datasets, streams.cqt_slices,
-#             t_len=t_len, batch_size=batch_size, use_zmq=True)
-#         yield __test_streamer, streamer, t_len, batch_size
+@pytest.mark.xfail(reason="zmq is always generating batches of only one.")
+def test_instrument_streamer_with_zmq(tiny_feats):
+    df = tiny_feats.to_df()
+    t_len = 10
+    batch_size = 12
+    if not df.empty:
+        streamer = streams.InstrumentStreamer(
+            df, streams.cqt_slices,
+            t_len=t_len, batch_size=batch_size, use_zmq=True)
+        __test_streamer(streamer, t_len, batch_size)
